d87242e30f0be3cb141786fb5cbbe1ef6414ccfe
ozner77 <nbravoser@gmail.com>
2024-10-17 00:29:56 -0400

    yes

---
+++ Codeforces/Codeforces_Round978/resueltos durante el contest/B_Kar_Salesman.cpp
@@ -0,0 +1,12 @@
+/*si alguien lee esto no me juzgue, me duele mi cabeza, no se en que estoy pensando, trataré de resolver mas en el upsolve*/
+#include <iostream>
+using namespace std;
+int main(){
+    long long t,a,b,c;
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        for(int i=0;i<b;i++){
+            
+        }
+    }
+}
