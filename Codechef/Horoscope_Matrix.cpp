44f4735bfd7c06a21733a623df7fe1f76f25d327
ozner77 <nbravoser@gmail.com>
2024-10-20 08:58:18 -0400

    update_codechef

--- Codechef/Horoscope_Matrix.cpp
+++ Codechef/Horoscope_Matrix.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    long long t,n,m,a;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>n>>m;
+        vector<vector<long long>> V;
+        for(int j=0;j<n;j++){
+            vector<long long> V1;
+            for(int k=0;k<m;k++){
+                cin>>a;
+                V1.push_back(a);
+            }
+            V.push_back(V1);
+        }
+        
+    }
 }
