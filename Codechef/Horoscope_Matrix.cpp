617f55217798a6671e64a85a98dceb804cedfb53
ozner77 <nbravoser@gmail.com>
2024-10-22 22:53:09 -0400

    codechef ejericico al 75% creo
    ya casii, pero me mandarona dormir pipipi

--- Codechef/Horoscope_Matrix.cpp
+++ Codechef/Horoscope_Matrix.cpp
@@ -5,7 +5,7 @@ int main(){
     cin>>t;
     for(int i=0;i<t;i++){
         cin>>n>>m;
-        long long sumas[n+m-1]={0};
+        vector<map<long long,long long>> C(n+m-1);
         vector<vector<long long>> V;
         for(int j=0;j<n;j++){
             vector<long long> V1;
@@ -17,11 +17,18 @@ int main(){
         }
         for(int j=0;j<n;j++){
             for(int k=0;k<m;k++){
-                sumas[k+(n-1)-j]+=V[j][k];
+                C[k+n-1-j][V[j][k]]++;
             }
         }
-        for(auto x:sumas){
-            cout<<x<<" ";
+        long long Q,x,y,z,mal=0;
+        while(Q--){
+            cin>>x>>y>>z;
+            C[y+n-1-x][V[x][y]]--;
+            C[y+n-1-x][z]++;
         }
     }
 }
+//no logre acabar pero tengo una idea en la que podría hacer un mapa por cada diagonal y luego comprobar que todos esten en 0 menos uno(del que va a estar compuesto la diagonal) y si es así esa diagonal esta bien, luego 
+//un contador que diga cuantas diagonales estan mal y si en la query el contador>0 entonces la matriz esta mal o si no esta bien, pero lo implementare mañana porque ya me esta doliendo la cabeza
+//pd: ya no haré los problemas a última hora
+//pd2: al final decidí avanzar lo más que pueda hoy 22 de octubre
