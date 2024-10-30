84564474f416f0cd823c9bf00516c11206303e99
ozner77 <nbravoser@gmail.com>
2024-10-30 15:01:02 -0400

    upsolve contest semana 2
    uy

---
+++ Codeforces/Codeforces_Round4(Nosimulado)/Watermelon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long esteesnunnombredevariableextremadamentelargouwu;
+    cin>>esteesnunnombredevariableextremadamentelargouwu;
+    if(esteesnunnombredevariableextremadamentelargouwu%2==0 && esteesnunnombredevariableextremadamentelargouwu>2){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
--- Codeforces/Codeforces_Round565(Simulado)/upsolve/Lose_it.cpp
+++ Codeforces/Codeforces_Round565(Simulado)/upsolve/Lose_it.cpp
@@ -1,5 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    map<long long, long long> M;
+    M[4]=0;
+    M[8]=0;
+    M[15]=0;
+    M[16]=0;
+    M[23]=0;
+    M[42]=0;
+    long long a,b,len=0,terminado=0;
+    cin>>a;
+    while(a--){
+        len++;
+        cin>>b;
+        if(b==4){
+            M[4]++;
+        }
+        if(b==8 && M[4]>0){
+            M[8]++;
+            M[4]--;
+        }
+        if(b==15 && M[8]>0){
+            M[15]++;
+            M[8]--;
+        }
+        if(b==16 && M[15]>0){
+            M[16]++;
+            M[15]--;
+        }
+        if(b==23 && M[16]>0){
+            M[23]++;
+            M[16]--;
+        }
+        if(b==42 && M[23]>0){
+            M[42]++;
+            M[23]--;
+        }
+    }
+    len=len-(M[42]*6);
+    cout<<len;
 }
+//NO PUEDO CREER QUE NO PUDE HACER ESTO TAN FÁCIL EN LA SIMULACIÓN :(
