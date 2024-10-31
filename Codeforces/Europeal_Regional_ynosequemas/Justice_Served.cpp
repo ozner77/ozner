ba9da1a8d9d97c0b25fca72da72fa47fa7441cae
ozner77 <nbravoser@gmail.com>
2024-10-31 17:30:02 -0400

    Justice Served al fin

--- Codeforces/Europeal_Regional_ynosequemas/Justice_Served.cpp
+++ Codeforces/Europeal_Regional_ynosequemas/Justice_Served.cpp
@@ -4,7 +4,7 @@ int main(){
     long long a,b,c,l,r,m;
     cin>>a;
     vector<vector<long long>> V;
-    vector<long long> xd(a,0);
+    vector<long long> xd;
     vector<long long> res(a,0);
     long long con=0;
     while(a--){
@@ -17,33 +17,46 @@ int main(){
         V.push_back(V1);
         con++;
     }
-    sort(V.begin(),V.end());
-    xd[0]=V[0][1];
+    sort(V.begin(), V.end(), [](vector<long long> a,vector<long long> b) {
+        if (a[0] != b[0]) {
+            return a[0] < b[0];
+        }
+        return a[1] > b[1];
+    });
+    xd.push_back(V[0][1]);
     res[V[0][2]]=0;
     con=0;
     for(auto x:V){
-        long long uwu=x[1];
-        l=0;
-        r=xd.size()-1;
+        if(con!=0){
+        long long l=0,r=xd.size()-1;
+        long long m;
         while (l <= r) {
             m = l + (r - l) / 2;
-            if (xd[m] > uwu) {
+            if (xd[m] == x[1]) {
                 l = m + 1;
+            }   
+            else if (xd[m]>x[1]){
+                l=m+1;
             } else {
-                r = m - 1;
+                r=m-1;
             }
         }
-        if(uwu>=xd[m]){
-            xd[m]=uwu;
-            res[V[con][2]]=m;
+        if(x[1]>xd[m]){
+            xd[m]=x[1];
+            res[x[2]]=m;
+        }else if(m==xd.size()-1){
+            xd.push_back(x[1]);
+            res[x[2]]=xd.size()-1;
         }else{
-            xd[m+1]=uwu;
-            res[V[con][2]]=m+1;
+            xd[m+1]=x[1];
+            res[x[2]]=m+1;
+        }
         }
         con++;
     }
     for(auto x:res){
-        cout<<x;
+      cout<<x<<" ";
     }
 }
-//agradecido por el likes, necesito dejar de dejar todo a ultima hora
+//AL FIN JUSTICE SERVED
+//muchas gracias ming :D
