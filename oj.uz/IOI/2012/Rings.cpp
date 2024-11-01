45b209b127a129e9e75d0429e660466a204e4e8e
ozner77 <nbravoser@gmail.com>
2024-10-31 23:57:57 -0400

    humildes avanzes en rings

--- oj.uz/IOI/2012/Rings.cpp
+++ oj.uz/IOI/2012/Rings.cpp
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<vector<long long>> V;
+int init(long long N){
+    vector<long long> V1(N,0);
+    for(int i=0;i<N;i++){
+        V.push_back(V1);
+    }
+}
 int Link(long long A,long long B){
-    //estaba pensando en union find pero ya veo que no me va a salir, no se como linkearlos :(
+    V[A][B]=1;
+    V[B][A]=1;
+    //estaba pensando en usar union find pero no se com linkearlos D:
 }
 int CountCritical(){
     long long critical;
     return critical;
 }
-int init(long long N){
-    
+int cadena(){
+    //si de por si es una cadena todos los anillos serían critical
+    bool respuesta=false;
+    return respuesta;
 }
