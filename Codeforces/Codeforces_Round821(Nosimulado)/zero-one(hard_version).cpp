9d2c2a30152fe8f013ceb39006cae4113857adce
ozner77 <nbravoser@gmail.com>
2024-10-31 22:32:40 -0400

    Traffic y zero one

---
+++ Codeforces/Codeforces_Round821(Nosimulado)/zero-one(hard_version).cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	long long t,mal[5002];
+	cin>>t;
+	while(t--) {
+		vector<long long> xd;
+		long long n,x,y;
+		cin>>n>>x>>y;
+		string a,b;
+		cin>>a>>b;
+		if(x>=y){
+			for(int i=0;i<n;i++){
+				if(a[i]!=b[i]){
+                xd.push_back(i);
+        	    }
+			}
+			if(xd.size()%2!=0){
+        	    cout<<-1<<"\n";
+        	}
+			else if(xd.size()==2) {
+				if(xd[0]+1==xd[1]) {
+					if(n!=2){
+            	        cout<<min(x,2*y)<<"\n";
+            	    }
+					else{
+            	        cout<<x<<"\n";
+            	    }
+				}
+				else{
+        	        cout<<y<<"\n";
+        	    }
+			}
+			else{
+        	    cout<<y*(xd.size())/2<<"\n";
+        	}
+		}else{
+			for(int i=0;i<n;i++){
+				if(a[i]!=b[i]){
+					xd.push_back(i);
+				}
+			}
+			if(xd.size()%2!=0){
+				cout<<-1<<endl;
+			}
+			else {
+				if(xd.size()==0) {
+					cout<<0<<endl;
+					continue;
+				}
+				mal[0]=0;
+				mal[1]=0;
+				for(int i=2;i<=xd.size();i++) {
+					mal[i]=mal[i-2]+min(x*(xd[i-1]-xd[i-2]),y);
+					if(i%2==0){
+						mal[i]=min(mal[i],mal[i-1]+y);
+					}
+					else{
+						mal[i]=min(mal[i],mal[i-1]);
+					}
+				}
+				cout<<mal[xd.size()]<<endl;
+			}
+		}
+    }
+}    
---
+++ oj.uz/IOI/2010/Traffic.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+ 
+#define _USE_MATH_DEFINES
+#define INF LLONG_MAX
+#define MOD 1000000007
+ 
+#define endl "\n"
+#define sp " "
+ 
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+ 
+#define f1(i, x) for(auto &i : x)
+#define f2(i, x, j) for(ll i = j; i < x; i++)
+ 
+#define raya() cout << endl << "====================================" << endl
+#define dbg(x) cerr << #x << ": " << x << endl;
+ 
+using namespace std;
+using ll = long long;
+ 
+int LocateCentre(int n, int p[], int s[], int d[]){
+    vector<int> pref(n + 1);
+    f2(i, n, 0){
+        pref[i + 1] = p[i] + pref[i];
+    }
+    int ansa = 0, ma = pref[n];
+    f2(i, n + 1, 1){
+        int x = pref[i - 1];
+        int y = pref[n] - pref[i];
+        int t = max(x, y);
+        if(t < ma){
+            ansa = i - 1;
+            ma = t;
+        }
+    }
+    return ansa;
+}
+int main(){
+    long long N=5;
+    int P[]={1,2,3,4,5};
+    int S[]={1,2,3,4,5};
+    int D[]={1,2,3,4,5};
+    long long res=LocateCentre(N,P,S,D);
+    cout<<res;
+}
