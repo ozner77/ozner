607a618a27875412e543d31ae07cbc086756f95d
ozner77 <nbravoser@gmail.com>
2024-11-05 21:29:39 -0400

    stove :D

--- oj.uz/JOI/2018/Stove.cpp
+++ oj.uz/JOI/2018/Stove.cpp
@@ -3,18 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    ll a,b,c;
+    ll a,b,c,ant;
     cin>>a>>b;
+    ll res=a;
+    vector<ll> V;
     if(b<a){
+        ll con=0;
         while(a--){
         cin>>c;
+        if(con>0){
+            V.push_back(c-ant);
         }
+        con++;
+        ant=c;
+        }
+        sort(V.begin(),V.end());
+        ll xd=res-b;
+        for(int i=0;i<xd;i++){
+            res--;
+            res+=V[i];
+        }
+        cout<<res;
     }else{
         while(a--){
         cin>>c;
         }
-        cout<<a;
+        cout<<res;
     }
 }
-//tengo una idea pero no se si funcionará xd
-//LO MÁS probable es que este mal pero soy yo o este problema esta muy facil, no sé 
